9c7226452987d2ae5cfa540ffae5379d15fa12ac
Khoa <52121010+11Khoa@users.noreply.github.com>
2026-03-11 02:14:26 +0000

    update

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-const { chromium } = require("playwright");
 const fs = require("fs");
 
 async function getPrices() {
+  const res = await fetch("https://www.petrolimex.com.vn/nd/bao-gia-xang-dau.html");
+  const html = await res.text();
 
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
+  const start = html.indexOf("window.__vieapps");
+  if (start === -1) throw new Error("Không tìm thấy vieapps");
 
-  await page.goto(
-    "https://www.petrolimex.com.vn/nd/bao-gia-xang-dau.html",
-    { waitUntil: "domcontentloaded" }
-  );
+  const jsonStart = html.indexOf("{", start);
+  const jsonEnd = html.indexOf("};", jsonStart) + 1;
 
-  await page.waitForFunction(() => window.__vieapps?.prices?.products);
+  const jsonStr = html.slice(jsonStart, jsonEnd);
+  const data = JSON.parse(jsonStr);
 
-  const products = await page.evaluate(() => {
-    return window.__vieapps.prices.products;
-  });
-
-  await browser.close();
-
-  return products;
+  return data.prices.products;
 }
 
 (async () => {
+  try {
+    const prices = await getPrices();
 
-  const prices = await getPrices();
+    fs.writeFileSync(
+      "prices.json",
+      JSON.stringify(prices, null, 2)
+    );
 
-  fs.writeFileSync(
-    "prices.json",
-    JSON.stringify(prices, null, 2)
-  );
-
-  console.log("prices.json created");
-
+    console.log(prices);
+    console.log("prices.json created");
+  } catch (err) {
+    console.error(err);
+  }
 })();
